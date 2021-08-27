2a884b245e6766b2417238d0c42e12a63beb5f0e
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-08-27 09:28:36 -0700

    update

--- src/App.js
+++ src/App.js
@@ -12,6 +12,53 @@ class App extends Component {
         }
     }
 
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        try {
+            this.setState({
+                // eslint-disable-next-line
+                result: (eval(this.state.result) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
     render() {
         return (
             <div>
